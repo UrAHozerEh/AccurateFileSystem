e84d97f82bc8590fd17eed5336a514e0c2e66651
Kevin Cronk <kevjcronk@gmail.com>
2020-04-03 14:15:12 -0700

    Added ssurveydirectionwithdateseries.

--- AccurateReportSystem/SurveyDirectionSeries.cs
+++ AccurateReportSystem/SurveyDirectionSeries.cs
@@ -75,23 +75,23 @@ namespace AccurateReportSystem
                 }
                 else
                 {
-                    foreach (var area in areasOnPage)
+                    foreach (var (start, end, isReverse) in areasOnPage)
                     {
-                        var direction = area.IsReverse ? ArrowInfo.Direction.Left : ArrowInfo.Direction.Right;
-                        if (area.Start < page.StartFootage)
+                        var direction = isReverse ? ArrowInfo.Direction.Left : ArrowInfo.Direction.Right;
+                        if (start < page.StartFootage)
                         {
-                            var x = transform.ToDrawArea(area.End);
+                            var x = transform.ToDrawArea(end);
                             ArrowInfo.DrawLeftOf(session, x, y, direction);
                             continue;
                         }
-                        if (area.End > page.EndFootage)
+                        if (end > page.EndFootage)
                         {
-                            var x = transform.ToDrawArea(area.Start);
+                            var x = transform.ToDrawArea(start);
                             ArrowInfo.DrawRightOf(session, x, y, direction);
                             continue;
                         }
-                        var startX = transform.ToDrawArea(area.Start);
-                        var endX = transform.ToDrawArea(area.End);
+                        var startX = transform.ToDrawArea(start);
+                        var endX = transform.ToDrawArea(end);
                         var middleX = (startX + endX) / 2;
                         var xWidth = endX - startX;
                         if (xWidth < ArrowInfo.Width)
@@ -110,6 +110,112 @@ namespace AccurateReportSystem
                         {
                             ArrowInfo.DrawRightOf(session, startX, y, direction);
                             ArrowInfo.DrawLeftOf(session, endX, y, direction);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public class SurveyDirectionWithDateSeries : ChartSeries
+    {
+        public override float Height => ArrowInfo.Height;
+        public List<(double, bool, string)> DirectionData { get; set; }
+        private List<(double Start, double End, bool IsReverse, string Date)> DirectionAreas;
+        public bool FancySingleMiddle { get; set; } = false;
+        public ArrowInfo ArrowInfo { get; set; }
+        public override bool IsDrawnInLegend
+        {
+            get { return false; }
+            set { throw new InvalidOperationException(); }
+        }
+        public override Color LegendNameColor { get => throw new ArgumentException(); set => throw new InvalidOperationException(); }
+
+        public SurveyDirectionWithDateSeries(List<(double, bool, string)> directionData)
+        {
+            DirectionData = directionData;
+
+            ArrowInfo = new ArrowInfo(0.45f, 0.25f, Colors.Black, Colors.White);
+            UpdateAreas();
+        }
+
+        public void UpdateAreas()
+        {
+            DirectionAreas = new List<(double, double, bool, string)>();
+            (double Start, double End, bool IsReverse, string Date)? curData = null;
+            foreach (var (footage, isReverse, date) in DirectionData)
+            {
+                if (!curData.HasValue)
+                {
+                    curData = (footage, footage, isReverse, date);
+                    continue;
+                }
+                var curDataValue = curData.Value;
+                if (curDataValue.IsReverse == isReverse && date.Equals(curDataValue.Date))
+                {
+                    curData = (curDataValue.Start, footage, isReverse, date);
+                }
+                else
+                {
+                    DirectionAreas.Add(curDataValue);
+                    curData = (footage, footage, isReverse, date);
+                }
+            }
+            if (curData.HasValue)
+                DirectionAreas.Add(curData.Value);
+        }
+
+        public override void Draw(PageInformation page, CanvasDrawingSession session, Rect drawArea, TransformInformation1d transform)
+        {
+            using (session.CreateLayer(1f, drawArea))
+            {
+                var areasOnPage = DirectionAreas.Where(area => area.Start < page.EndFootage && area.End > page.StartFootage).ToList();
+                var y = (float)(drawArea.Top + Math.Round(drawArea.Height / 2, GraphicalReport.DIGITS_TO_ROUND));
+                if (areasOnPage.Count == 0)
+                    return;
+                if (areasOnPage.Count == 1)
+                {
+                    var (_, _, value, date) = areasOnPage[0];
+                    var middleX = drawArea.GetMiddlePoint().X;
+                    ArrowInfo.DrawOnTopOf(session, middleX, y, value ? ArrowInfo.Direction.Left : ArrowInfo.Direction.Right, date);
+                }
+                else
+                {
+                    foreach (var (start, end, isReverse, date) in areasOnPage)
+                    {
+                        var direction = isReverse ? ArrowInfo.Direction.Left : ArrowInfo.Direction.Right;
+                        if (start < page.StartFootage)
+                        {
+                            var x = transform.ToDrawArea(end);
+                            ArrowInfo.DrawLeftOf(session, x, y, direction, date);
+                            continue;
+                        }
+                        if (end > page.EndFootage)
+                        {
+                            var x = transform.ToDrawArea(start);
+                            ArrowInfo.DrawRightOf(session, x, y, direction, date);
+                            continue;
+                        }
+                        var startX = transform.ToDrawArea(start);
+                        var endX = transform.ToDrawArea(end);
+                        var middleX = (startX + endX) / 2;
+                        var xWidth = endX - startX;
+                        if (xWidth < ArrowInfo.Width)
+                        {
+                            var xScale = xWidth / ArrowInfo.Width;
+                            var arrowWidthInches = ArrowInfo.WidthInches;
+                            ArrowInfo.WidthInches *= xScale;
+                            ArrowInfo.DrawOnTopOf(session, middleX, y, direction, date);
+                            ArrowInfo.WidthInches = arrowWidthInches;
+                        }
+                        else if (xWidth < ArrowInfo.Width * 2)
+                        {
+                            ArrowInfo.DrawOnTopOf(session, middleX, y, direction, date);
+                        }
+                        else
+                        {
+                            ArrowInfo.DrawRightOf(session, startX, y, direction, date);
+                            ArrowInfo.DrawLeftOf(session, endX, y, direction, date);
                         }
                     }
                 }
